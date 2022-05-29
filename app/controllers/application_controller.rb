d2f5d8366af90df2266f9230b8868beef0ae497b
Alejandro Ortega <43015395+AlejoOrtega@users.noreply.github.com>
2022-05-29 17:14:09 -0400

    BackEnd Log In Done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,38 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
+  # GET ROUTES
   get "/" do
-    { message: "Good luck with your project!" }.to_json
+    { message: "Welcome to flappy bird sinatra" }.to_json
+  end
+
+  get "/users" do
+    response = User.all
+    response.to_json
+  end
+
+  get "/users/:id" do
+    response = User.find(params[:id])
+    response.to_json
+  end
+
+  # POST ROUTES
+  post "/users/login" do
+    if User.exists?(username: params[:username], password: params[:password])
+      response = User.where(username: params[:username], password: params[:password])
+    else
+      response = false
+    end
+    response.to_json(:only => [ :id, :username ])
+  end
+
+  post '/users' do
+    if User.exists?(username: params[:username])
+      response = false
+    else
+      response = User.create(username: params[:username], password: params[:password])
+    end
+    response.to_json(:only => [ :id, :username ])
   end
 
 end
---
+++ db/migrate/20220529162313_create_scores_table.rb
@@ -0,0 +1,9 @@
+class CreateScoresTable < ActiveRecord::Migration[6.1]
+  def change
+    create_table :scores do |t|
+      t.integer :user_id
+      t.integer :score
+      t.timestamps
+    end
+  end
+end
