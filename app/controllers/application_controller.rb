7fc0ad7efbe6f1ecb46b2d04cbaf324768a26df3
Alejandro Ortega <43015395+AlejoOrtega@users.noreply.github.com>
2022-05-30 21:07:56 -0400

    All endpoints complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,18 +2,19 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # GET ROUTES
-  get "/" do
-    { message: "Welcome to flappy bird sinatra" }.to_json
-  end
 
-  get "/users" do
-    response = User.all
+  get "/scores" do
+    response = Score.joins("inner join users on scores.user_id = users.id").select('users.username, scores.score, users.created_at').order('score DESC').take(20)
     response.to_json
   end
 
-  get "/users/:id" do
-    response = User.find(params[:id])
-    response.to_json
+  get "/users/:userid/scores" do
+    response = User.find(params[:userid]).scores.order("score DESC")
+    if response.any?
+     response.to_json(:only => [ :user_id, :score, :created_at ])
+    else
+      response.to_json
+    end
   end
 
   # POST ROUTES
@@ -38,5 +39,23 @@ class ApplicationController < Sinatra::Base
   post '/score' do
     response = Score.create(score: params[:score], user_id: params[:id])
     response.to_json
+  end
+
+  #PATCH ROUTES
+  patch '/users/:id' do
+    if User.exists?(username: params[:username])
+      response = false
+    else
+      User.find(params[:id]).update(username: params[:username])
+    end
+    response.to_json(:only => [ :id, :username ])
+  end
+
+  #DELETE ROUTES
+
+  delete '/users/:id' do
+    response = User.find(params[:id]).destroy
+    response.to_json
+
   end
 end
